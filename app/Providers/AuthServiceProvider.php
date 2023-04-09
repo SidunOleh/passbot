a38c16c61715a843a493e4a64aff832430c5faec
Oleh Sidun <sidunoleh@gmail.com>
2023-04-09 19:52:10 +0300

    change

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use TelegramBot\Api\Client as TgClient;
 
 class AuthServiceProvider extends ServiceProvider
 {
