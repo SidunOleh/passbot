0ab99253c04599a5fa264a6ca6c11716bbb483cc
Oleh Sidun <sidunoleh@gmail.com>
2023-04-09 23:28:19 +0300

    change

--- config/tg.php
+++ config/tg.php
@@ -4,7 +4,7 @@ return [
     'passbot' => [
         'token' => '6043059659:AAH5juQyosOVq8VmKQVObdhln7gC38aknWA',
         'users_ids' => [
-            // 515134223,
+            515134223,
         ],
     ],
 ];
