93f27e84369f6045006d8654e201466cfca69632
Oleh Sidun <sidunoleh@gmail.com>
2023-04-09 18:39:48 +0300

    change

--- app/Tg/Commands/HelpCommand.php
+++ app/Tg/Commands/HelpCommand.php
@@ -2,10 +2,14 @@
 
 namespace App\Tg\Commands;
 
+use Illuminate\Support\Facades\Log;
+
 class HelpCommand extends TgCommand
 {
     public function handle($message): void
     {
+        Log::info($message);
+
         $this->bot->sendMessage(
             $message->getChat()->getId(), 
             view('tg.commands')->render()
