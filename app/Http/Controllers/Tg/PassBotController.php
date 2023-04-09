3202a2d0747ae04f22c4835028da514db6997576
Oleh Sidun <sidunoleh@gmail.com>
2023-04-09 20:24:46 +0300

    change

--- app/Http/Controllers/Tg/PassBotController.php
+++ app/Http/Controllers/Tg/PassBotController.php
@@ -7,33 +7,21 @@ use App\Tg\Commands\HelpCommand;
 use App\Tg\Commands\SitesCommand;
 use Closure;
 use TelegramBot\Api\Client as TgClient;
-use Illuminate\Support\Facades\Gate;
 
 class PassBotController extends Controller
 {
     private $passbotToken;
     
     private $passbot;
-
-    private $input;
     
     public function __construct()
     {
         $this->passbotToken = config('tg.passbot.token');
         $this->passbot = new TgClient($this->passbotToken);
-        $this->input = json_decode(file_get_contents('php://input'), true);
     }
     
     public function __invoke()
     {
-        if (! $this->allowAccess()) {
-            $this->passbot->sendMessage(
-                $this->input['message']['chat']['id'],
-                'Access Denied. Text to @qwerty_sova to get access.',
-            );
-            die;
-        }
-
         $this->passbot->command('start', Closure::fromCallable([
             new HelpCommand($this->passbotToken), 
             'handle'
@@ -48,13 +36,5 @@ class PassBotController extends Controller
         ]));
 
         $this->passbot->run();
-    }
-
-    private function allowAccess()
-    {
-        return in_array(
-            $this->input['message']['from']['username'], 
-            config('tg.passbot.users')
-        );
     }
 }
---
+++ app/Http/Middleware/AuthPassBot.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use TelegramBot\Api\Client as TgClient;
+
+class AuthPassBot
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $input = json_decode(file_get_contents('php://input'), true);
+        $username = $input['message']['from']['username'];
+        if (in_array($username, config('tg.passbot.users'))) {
+            return $next($request);
+        }
+        
+        $passbot = new TgClient($this->passbotToken);
+        $passbot->sendMessage(
+            $input['message']['chat']['id'],
+            'Access Denied. Text to @qwerty_sova to get access.'
+        );
+    }
+}
