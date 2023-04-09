6e5287d31126a0ed1dbff8668edd58ad3021b110
Oleh Sidun <sidunoleh@gmail.com>
2023-04-09 19:27:54 +0300

    change

--- app/Tg/Commands/HelpCommand.php
+++ app/Tg/Commands/HelpCommand.php
@@ -9,8 +9,8 @@ class HelpCommand extends TgCommand
         $input = json_decode(file_get_contents('php://input'), true);
         $username = $input['message']['from']['username'];
         if ( ! in_array($username, config('tg.passbot.users')) ) {
-            $passbot = new TgClient(config('tg.passbot.token'));
-            $passbot->sendMessage($input['chat']['id'], 'Not allowed, text to @querty_sova to get access.');
+            // $passbot = new TgClient(config('tg.passbot.token'));
+            $this->bot->sendMessage($input['chat']['id'], 'Not allowed, text to @querty_sova to get access.');
             // return;
         } else {
                $this->bot->sendMessage(
