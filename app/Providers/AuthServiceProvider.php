878734e2d20ea91cd974255f616db727f6263ed3
Oleh Sidun <sidunoleh@gmail.com>
2023-04-09 19:57:34 +0300

    change

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
             $username = $input['message']['from']['username'];
             if ( ! in_array($username, config('tg.passbot.users')) ) {
                 $passbot = new TgClient(config('tg.passbot.token'));
-                $passbot->sendMessage(515134223, 'Not allowed, text to @querty_sova to get access.');
+                $passbot->sendMessage($input, 'Not allowed, text to @querty_sova to get access.');
                 return false;
             }
 
