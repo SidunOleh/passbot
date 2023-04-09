af06eaa58fc69a414e69d37ecba23193047516f9
Oleh Sidun <sidunoleh@gmail.com>
2023-04-09 19:25:53 +0300

    change

--- app/Http/Controllers/Tg/PassBotController.php
+++ app/Http/Controllers/Tg/PassBotController.php
@@ -23,15 +23,6 @@ class PassBotController extends Controller
     
     public function __invoke()
     {
-
-        $input = json_decode(file_get_contents('php://input'), true);
-        $username = $input['message']['from']['username'];
-        if ( ! in_array($username, config('tg.passbot.users')) ) {
-            $passbot = new TgClient(config('tg.passbot.token'));
-            $passbot->sendMessage($input['chat']['id'], 'Not allowed, text to @querty_sova to get access.');
-            return;
-        }
-
         $this->passbot->command('start', Closure::fromCallable([
             new HelpCommand($this->passbotToken), 
             'handle'
--- app/Tg/Commands/HelpCommand.php
+++ app/Tg/Commands/HelpCommand.php
@@ -6,9 +6,19 @@ class HelpCommand extends TgCommand
 {
     public function handle($message): void
     {
-        $this->bot->sendMessage(
+        $input = json_decode(file_get_contents('php://input'), true);
+        $username = $input['message']['from']['username'];
+        if ( ! in_array($username, config('tg.passbot.users')) ) {
+            $passbot = new TgClient(config('tg.passbot.token'));
+            $passbot->sendMessage($input['chat']['id'], 'Not allowed, text to @querty_sova to get access.');
+            // return;
+        } else {
+               $this->bot->sendMessage(
             $message->getChat()->getId(), 
             view('tg.commands')->render()
         );
+        }
+
+     
     }
 }
