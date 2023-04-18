f93f196a408017536a07d6dcb81d15d89c41c2d4
Oleh Sidun <sidunoleh@gmail.com>
2023-04-19 01:00:59 +0300

    change

--- resources/views/tg/sites.blade.php
+++ resources/views/tg/sites.blade.php
@@ -1,8 +1,7 @@
 @if (count($sites))
 @foreach ($sites as $site)
 {{ $site->name }}
-<a href="/sites {{ $site->name }}">/sites {{ $site->name }}</a>
-"/sites {{ $site->name }}"
+
 {{ $site->url }}
 
 =================================================
